589cf1015cd75984aaf0cf2f4ba21db0d767beb9
yueleilei <yueleilei2004@163.com>
2019-05-14 21:21:13 +0800

    add birthday.py & name.py
    book chapter 2

---
+++ name.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  name.py
+#  
+#  Copyright 2019 YUE <YUE@YLL>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+# 2019-05-14 
+
+name = "ada lovelace"
+print(name.title())
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "Lovelace"
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
+print("Python")
+print("\tPython")
+print("Language:\nPython\nC\nJavaScript")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+
